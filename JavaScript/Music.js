9e34c40059f69a5466e1fdc2d4c3ec9390fae928
Amisha Yadav <amishaydv0108@gmail.com>
2026-07-15 22:15:50 +0530

    JavaScript Folder

---
+++ JavaScript/Music.js
@@ -0,0 +1,88 @@
+let allMusic = [
+  {
+    name: "Shaabaashiyaan",
+    artist: "Shilpa Rao,Anand Bhaskar,Abhijeet Srivastava",
+    img: "Shabasshiyan song.jpeg",
+    src: "Shaabaashiyaan",
+  },
+
+  {
+    name: "Aarambh Hai Prachand",
+    artist: "Piyush Mishra",
+    img: "Aarambh Hai Prachand song.jpeg",
+    src: "Aarambh-Hai-Prachand",
+  },
+
+  {
+    name: "Besabriyaan",
+    artist: "Armaan Malik,Amaal Mallik",
+    img: "Beshabriyaan song.jpeg",
+    src: "Besabriyaan",
+  },
+  {
+    name: "O Shera - Teer Te Taj",
+    artist: "Manmohan Waris Kamal,Heer Sangtar,Shashwat Sachdev",
+    img: "O Shera-Teer Te Taj.jpeg",
+    src: "O-Shera-Teer-Te-Taj",
+  },
+  {
+    name: "Dil Mein Mars Hai",
+    artist: "Benny Dayal,Vibha Saraf",
+    img: "Dil-Mein-Mars-Hai.jpg",
+    src: "Dil-Me-Mars-Hai",
+  },
+
+  {
+    name: "Desh Mere",
+    artist: "Arijit Singh",
+    img: "Desh Mere song.jpeg",
+    src: "DESH-MERE",
+  },
+
+  {
+    name: "Lehra Do",
+    artist: "Arijit Singh",
+    img: "Lehra-Do song.jpeg",
+    src: "Lehra-Do",
+  },
+
+  {
+    name: "Ziddi Dil",
+    artist: "Vishal Dadlani",
+    img: "Ziddi Diil song.jpeg",
+    src: "Ziddi-Dil",
+  },
+
+  {
+    name: "Zinda",
+    artist: "Siddharth Mahadevan",
+    img: "Zinda song.jpeg",
+    src: "Zinda",
+  },
+
+  {
+    name: "Kar Har Maidaan Fateh",
+    artist: " Shreya Ghoshal,Sukhwinder Singh",
+    img: "Kar har Maidan Fateh.jpeg",
+    src: "Kar-Har-Maidan-Fateh",
+  },
+  {
+    name: "Chak De India",
+    artist: "Sukhwinder Singh,Salim Merchant,Marianne D'Cruz",
+    img: "Chak De India song.jpeg",
+    src: "Chak-De-India",
+  },
+
+  {
+    name: "Aashayein",
+    artist: "KK,Salim–Sulaiman",
+    img: "Aashayein.jpg",
+    src: "Aashayein",
+  },
+  {
+    name: "Bandeya Re Bandeya",
+    artist: "Arijit Singh and Asees Kaur",
+    img: "Bandeya-re-bandeya.jpeg",
+    src: "Bandeya-Re-Bandeya",
+  },
+];
